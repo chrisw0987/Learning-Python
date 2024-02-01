e3958b3a4aa9a5c316fd97f93cddeb781e067075
chrisw0987 <152220258+chrisw0987@users.noreply.github.com>
2024-02-01 14:12:44 -0500

    Add files via upload

---
+++ LEARNING PYTHON/Classes & Objects/Student.py
@@ -0,0 +1,7 @@
+class Student:
+
+    def __init__(self, name, major, gpa, is_on_probation):
+        self.name = name                                   #the name of the object is what the user passes in
+        self.major = major                                 #the major of the object is what the user passes in
+        self.gpa = gpa                                     #the major of the object is what the user passes in
+        self.is_on_probation = is_on_probation             #the major of the object is what the user passes in
---
+++ LEARNING PYTHON/Classes & Objects/main.py
@@ -0,0 +1,5 @@
+from Student import Student
+
+student1 = Student("Chris", "TSM", 3.0, False)
+
+print(student1.is_on_probation)
